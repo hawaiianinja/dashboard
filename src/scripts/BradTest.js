506b917468d27ee2bba3fb58053e5cf61b407a2e
Brad Gough <brad.m.gough@gmail.com>
2016-09-30 21:04:02 -1000

    Added Brad and Jordan Test Pages

---
+++ src/scripts/BradTest.js
@@ -0,0 +1,27 @@
+﻿// Import modules used by this module
+import View from './View';
+
+// Declare and export this module
+var Test = new View('Brad Test', 'BradTest.html');
+export default Test;
+
+// Excude Immediately-Invoked Function Expressions (IIFE) to prevent creation of globally scoped variables
+(function(){
+
+    // Declare module scoped variables
+
+    // Declare module scoped functions
+
+    // Code to run before view is loaded (i.e. before $(document).ready function)
+    Test.beforeLoad = function (options) {
+        console.log('beforeLoad');
+        console.log(options);
+    };
+
+    // Code to run before view is loaded (i.e. after $(document).ready function)
+    Test.afterLoad = function (options) {
+        console.log('afterLoad');
+        console.log(options);
+    };
+
+})();
